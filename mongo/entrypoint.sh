ca7f353d2ebce5576ca6ed3c038cfe2eaf08270e
Jai <jbibar0905@gmail.com>
2021-03-17 14:47:33 +0800

    Initial commit

---
+++ mongo/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# set -e
+
+export KOBO_DOCKER_SCRIPTS_DIR=/kobo-docker-scripts
+
+# Send backup installation process in background to avoid blocking MongoDB startup
+bash $KOBO_DOCKER_SCRIPTS_DIR/toggle-backup-activation.sh &
+
+echo "Copying init scripts ..."
+cp $KOBO_DOCKER_SCRIPTS_DIR/init_* /docker-entrypoint-initdb.d/
+
+bash $KOBO_DOCKER_SCRIPTS_DIR/upsert_users.sh
+
+echo "Launching official entrypoint..."
+# `exec` here is important to pass signals to the database server process;
+# without `exec`, the server will be terminated abruptly with SIGKILL (see #276)
+exec bash /entrypoint.sh mongod
